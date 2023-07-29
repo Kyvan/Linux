922863cc2059845f6a63058635b65f8b5a5b4bf6
Kyvan Emami <emamikyvan@gmail.com>
2023-07-29 08:58:49 -0400

    Simplified the script

--- pkg.sh
+++ pkg.sh
@@ -4,80 +4,63 @@
 version=/proc/version
 green='\033[1;32m'
 brown='\033[1;33m'
+cyan='\033[1;36m'
 blue='\033[1;34m'
 NC='\033[0m' # No Color
 
-function debian_update() {
-    echo -e "${blue}Checking updates...${NC}"
-    sudo apt "${1}"
-    echo -e "${green}Checking for updates done...${NC}"
-    echo -e "${brown}Installing updates (if applicable)...${NC}"
-    sudo apt "${2}"
-    echo -e "${green}Installing updates done...${NC}"
-}
-
-function rhel() {
+function pkg_update() {
     echo -e "${blue}Checking and installing updates...${NC}"
-    sudo dnf "${1}" "${2}" 
+    sudo "${pkg}" update
     echo -e "${green}Checking and installing updates done...${NC}"
 }
 
-function suse() {
-    echo -e "${blue}Checking and installing updates...${NC}"
-    sudo zypper "${1}" "${2}"
-    echo -e "${green}Checking and installing updates done...${NC}"
+function pkg_update_deb() {
+    echo -e "${blue}Checking for updates...${NC}"
+    sudo "${pkg}" update
+    echo -e "${brown}installing updates...${NC}"
+    sudo "${pkg}" upgrade
+    echo -e "${green}installing updates done...${NC}"
 }
 
-function debian_install() {
+function pkg_install() {
     echo -e "${blue}Installing new packages...${NC}"
-    sudo apt "${state}" "${package}"
+    sudo "${pkg}" "${1}" "${2}"
     echo -e "${green}Installing new packages done...${NC}"
-}
-
-function distro_check() {
-    # If statement to check which distro is used
-    if grep -iE '(debian|ubuntu)' "${version}" > /dev/null; then
-        debian_update "${1}" "${2}"
-    elif grep -iE '(red had)' "${version}" > /dev/null ; then
-        rhel "${1}"
-    elif grep -iE '(suse)' "${version}" > /dev/null ; then
-        suse "${1}"
-    else
-        echo "Why you being difficult?"
-        echo "Do your shit manually."
-    fi
 }
 
 function input_check() {
     if [[ "${1}" == "install" || "${1}" == "remove" ]] ; then
         state="${1}"
         package="${2}"
-        debian_install "${state}" "${package}"
+        pkg_install "${state}" "${package}"
     elif [[ "${2}" == "install" || "${2}" == "remove" ]] ; then
         state="${2}"
         package="${1}"
-        debian_install "${state}" "${package}"
+        pkg_install "${state}" "${package}"
     else
         echo "ERROR: One of the arguments passed needs to be \"install\" or \"remove\"."
         exit 1
     fi
 }
 
-
-
-if [[ $# -eq 0 ]] ; then
-    distro_check update upgrade
-elif [[ $# -eq 2 ]] ; then
-    input_check "${1}" "${2}"
-else
-    echo "ERROR: You need either zero or two option for the script."
-    echo "USAGE: $0 [argument]"
-    exit 1
-fi
+function distro_check() {
+    # If statement to check which distro is used
+    if grep -iE '(debian|ubuntu)' "${version}" > /dev/null; then
+        pkg="apt"
+    elif grep -iE '(red hat)' "${version}" > /dev/null ; then
+        pkg="dnf"
+    elif grep -iE '(suse)' "${version}" > /dev/null ; then
+        pkg="zypper"
+    else
+        echo "Why you being difficult?"
+        echo "Do your shit manually."
+    fi
+}
 
 # checking for Flatpak
 function flatpakCheck() {
-    read -rp "would you like to check for flatpack udpates? (YES/Y/no/n) -- Default is YES" flatAnswer
+    echo -e "${blue}would you like to check for flatpack udpates? (YES/Y/no/n)${NC} -- ${cyan}Default is YES${NC}"
+    read -r flatAnswer 
     case "${flatAnswer,,}" in
         y | yes | '')
             flatpak update
@@ -92,5 +75,20 @@ function flatpakCheck() {
     esac
 }
 
-#read -rp "would you like to check for flatpack udpates as well? (y/n)" flatAnswer
+distro_check
+
+if [[ $# -eq 0 ]] ; then
+    if grep -iE '(debian|ubuntu)' "${version}" > /dev/null ; then
+        pkg_update_deb
+    else
+        pkg_update
+    fi
+elif [[ $# -eq 2 ]] ; then
+    input_check "${1}" "${2}"
+else
+    echo "ERROR: You need either zero or two option for the script."
+    echo "USAGE: $0 [argument]"
+    exit 1
+fi
+
 flatpakCheck
