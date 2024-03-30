ae087c40a7718cc8c9cfd36d7d963e7aa4add177
Kyvan Emami <emamikyvan@gmail.com>
2024-03-30 11:28:09 -0400

    AUR installation script

---
+++ aur.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -ux
+
+targetDIR="$HOME/Apps"
+package="${1}"
+installDIR=$(echo $package | awk -F/ '{print $NF}' | awk -F\. '{print $1}')
+
+cd "${targetDIR}" || exit 1
+git clone "${package}"
+cd "${installDIR}" || exit 1
+makepkg
+
+for target in *.tar.zst ; do
+    sudo pacman -U "${target}"
+done
