45e3ff588087028f00438c6fc65729a92c183e67
Kyvan Emami <emamikyvan@gmail.com>
2024-04-17 19:52:52 -0400

    added an option to aur.sh for installing dependencies

--- aur.sh
+++ aur.sh
@@ -7,7 +7,7 @@ installDIR=$(echo $package | awk -F/ '{print $NF}' | awk -F\. '{print $1}')
 cd "${targetDIR}" || exit 1
 git clone "${package}"
 cd "${installDIR}" || exit 1
-makepkg
+makepkg -s
 
 for target in *.tar.zst ; do
     sudo pacman -U "${target}"
