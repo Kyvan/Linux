b5f9f3dfe1ba0b6b95a1a1e45bc931166b1c797a
Kyvan Emami <emamikyvan@gmail.com>
2026-01-11 10:03:08 -0500

    cleaned up TOKEN Output

--- wazuhCleaner.sh
+++ wazuhCleaner.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 ## Grab Authorization token
-TOKEN=$(curl -u wazuh-wui:Reproduction38Assignment\? -k -X POST "https://fedora:55000/security/user/authenticate")
+TOKEN=$(curl -u wazuh-wui:Reproduction38Assignment\? -k -X POST "https://wazuh:55000/security/user/authenticate?raw=true")
 
 ## Delete disconnected agents older than 15 days
-curl -k -X DELETE "https://fedora:55000/agents?older_than=1d&agents_list=all&status=disconnected&pretty=true" -H "Authorization: Bearer ${TOKEN}"
-curl -u wazuh-wui:Reproduction38Assignment\? -k -X POST "https://fedora:55000/security/user/authenticate"
+curl -k -X DELETE "https://wazuh:55000/agents?older_than=15d&agents_list=all&status=disconnected&pretty=true" -H "Authorization: Bearer ${TOKEN}"
